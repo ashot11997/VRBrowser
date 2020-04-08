f6bca82cb6251b83d1ec897c27562f5de580d065
Ashot Danielyan <ashot111997@gmail.com>
2020-04-08 16:00:04 +0400

    uploading

---
+++ VR Browser/Assets/Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button OpenBtn;
+    public Button CloseBtn;
+
+    public GameObject Browser;
+
+    public Animator Anim;
+
+    void Awake() {
+        OpenBtn.onClick.AddListener(Open);
+        CloseBtn.onClick.AddListener(Close);
+    }
+
+    void Open() {
+        Browser.SetActive(true);
+        Anim.Play("OpenAnim");
+        CloseBtn.interactable = true;
+        OpenBtn.interactable = false;
+    }
+
+    void Close()
+    {
+        Anim.Play("CloseAnim");
+        StartCoroutine(Closing());
+        CloseBtn.interactable = false;
+        OpenBtn.interactable = true;
+    }
+
+    IEnumerator Closing() {
+        yield return new WaitForSeconds(0.4f);
+        Browser.SetActive(false);
+    }
+}
